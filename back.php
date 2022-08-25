262b82749ac0e15db901221297d71ddad33b97db
o2965905 <o2965905@gmail.com>
2022-08-25 23:05:40 +0800

    建立前台畫面(瀏覽人數/include頁面功能/顯示今日日期/跑馬燈)並複製一份back.php

--- back.php
+++ back.php
@@ -29,9 +29,9 @@ include_once "./base.php";
 		</div>
 		<div id="mm">
 			<div class="hal" id="lef">
-				<a class="blo" href="?do=main">回首頁</a>
-				<a class="blo" href="?do=news">最新文章</a>
-				<a class="blo" href="?do=que">問卷調查</a>
+				<a class="blo" href="?do=user">帳號管理</a>
+				<a class="blo" href="?do=news">最新文章管理</a>
+				<a class="blo" href="?do=que">問卷管理</a>
 			</div>
 			<div class="hal" id="main">
 				<div>
@@ -42,11 +42,11 @@ include_once "./base.php";
 					<div class="content">
 						<?php
 						$do=$_GET['do']??'main';
-						$file="./front/".$do.".php";
+						$file="./back/".$do.".php";
 						if(file_exists($file)){
 							include $file;
 						}else{
-							include "./front/main.php";
+							include "./back/main.php";
 						}
 						?>
 					</div>
